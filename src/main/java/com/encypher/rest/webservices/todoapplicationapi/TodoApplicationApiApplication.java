173ff2b4bc84fcdaa7c4d9ced3d5d66a11ce79b0
Anthony Villegas <villegasanthony87@yahoo.com>
2024-02-15 01:22:12 -0600

    Enabled WebMvcConfigurer for CORS request for Spring Boot REST API

--- src/main/java/com/encypher/rest/webservices/todoapplicationapi/TodoApplicationApiApplication.java
+++ src/main/java/com/encypher/rest/webservices/todoapplicationapi/TodoApplicationApiApplication.java
@@ -2,12 +2,27 @@ package com.encypher.rest.webservices.todoapplicationapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TodoApplicationApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TodoApplicationApiApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("*")
+                        .allowedOrigins("http://localhost:3000");
+            }
+        };
+
     }
 
 }

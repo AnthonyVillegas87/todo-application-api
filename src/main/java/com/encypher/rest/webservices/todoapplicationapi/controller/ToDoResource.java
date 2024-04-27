62a78fae9b366c99142cf31e3f17a0f59ebd0a8d
Anthony Villegas <villegasanthony87@yahoo.com>
2024-04-27 18:23:23 -0500

    added UPDATE & CREATE REST API to SpringBoot Backend API

--- src/main/java/com/encypher/rest/webservices/todoapplicationapi/controller/ToDoResource.java
+++ src/main/java/com/encypher/rest/webservices/todoapplicationapi/controller/ToDoResource.java
@@ -2,20 +2,21 @@ package com.encypher.rest.webservices.todoapplicationapi.controller;
 
 import com.encypher.rest.webservices.todoapplicationapi.service.TodoService;
 import com.encypher.rest.webservices.todoapplicationapi.user.Todo;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.HandlerMapping;
 
 import java.util.List;
 
 @RestController
 public class ToDoResource {
+    private final HandlerMapping defaultServletHandlerMapping;
     private TodoService todoService;
 
-    public ToDoResource(TodoService todoService) {
+    public ToDoResource(TodoService todoService, @Qualifier("defaultServletHandlerMapping") HandlerMapping defaultServletHandlerMapping) {
        this.todoService = todoService;
+        this.defaultServletHandlerMapping = defaultServletHandlerMapping;
     }
 
     @GetMapping("/users/{username}/todos")
@@ -32,5 +33,17 @@ public class ToDoResource {
     public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable int id) {
         todoService.deleteById(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/users/{username}/todos/{id}")
+    public Todo updateTodo(@PathVariable String username, @PathVariable int id, @RequestBody Todo todo) {
+       todoService.update(todo);
+       return todo;
+    }
+
+    @PostMapping("/users/{username}/todos")
+    public Todo createTodo(@PathVariable String username,  @RequestBody Todo todo) {
+        Todo createdTodo = todoService.addTodo(username, todo.getDescription(), todo.getTargetDate(), todo.isDone() );
+        return createdTodo;
     }
 }

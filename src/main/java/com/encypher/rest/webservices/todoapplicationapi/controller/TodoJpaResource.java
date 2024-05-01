faaa8dc87fd163f5ff5b8cc89d0b437f081465f7
Anthony Villegas <villegasanthony87@yahoo.com>
2024-04-30 22:56:57 -0500

    practice with JPA & Hibernate - Connected to H2 in memory DB

--- src/main/java/com/encypher/rest/webservices/todoapplicationapi/controller/TodoJpaResource.java
+++ src/main/java/com/encypher/rest/webservices/todoapplicationapi/controller/TodoJpaResource.java
@@ -20,28 +20,36 @@ public class TodoJpaResource {
 
     @GetMapping("/users/{username}/todos")
     public List<Todo> retrieveTodoList(@PathVariable String username) {
-        return todoService.findByUsername(username);
+        //return todoService.findByUsername(username);
+
+        return todoRepository.findByUsername(username);
     }
 
     @GetMapping("/users/{username}/todos/{id}")
     public Todo retrieveTodo(@PathVariable String username, @PathVariable int id) {
-        return todoService.findById(id);
+        //return todoService.findById(id);
+
+        return todoRepository.findById(id).get();
     }
 
     @DeleteMapping("/users/{username}/todos/{id}")
     public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable int id) {
-        todoService.deleteById(id);
+        todoRepository.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 
     @PutMapping("/users/{username}/todos/{id}")
     public Todo updateTodo(@PathVariable String username, @PathVariable int id, @RequestBody Todo todo) {
-        todoService.update(todo);
+        //todoService.update(todo);
+        todoRepository.save(todo);
         return todo;
     }
 
     @PostMapping("/users/{username}/todos")
     public Todo createTodo(@PathVariable String username, @RequestBody Todo todo) {
-        return todoService.addTodo(username, todo.getDescription(), todo.getTargetDate(), todo.isDone());
+        todo.setUsername(username);
+        todo.setId(null);
+        return todoRepository.save(todo);
+        //return todoService.addTodo(username, todo.getDescription(), todo.getTargetDate(), todo.isDone());
     }
 }
